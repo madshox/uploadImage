c47db57a72368b4180e377a55a647db7884c5a8f
madshox <63535913+madshox@users.noreply.github.com>
2022-04-20 10:14:00 +0500

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+## learn do packages for laravel
+
+#### Install
+
+- composer require madshox/uploadImages
---
+++ src/Controllers/UploadImageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Madshox\UploadImages\Controllers;
+
+use Illuminate\Http\Request;
+
+class UploadImageController extends Controller
+{
+    public function uploadImage()
+    {
+        dd('dafs');
+    }
+}
